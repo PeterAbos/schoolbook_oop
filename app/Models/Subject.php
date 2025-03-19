d1019b0476a54a9b44c0975d84b72dfef0c496de
PeterAbos <abos.petike@gmail.com>
2025-03-19 15:17:49 +0100

    subject class

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -6,5 +6,13 @@ class Subject extends Model
 {
     public string|null $name = null;
 
+    protected static $table = 'subjects';
 
+    public function __construct(?string $name = null)
+    {
+        parent::__construct();
+        if ($name) {
+            $this->name = $name;
+        }
+    }
 }
