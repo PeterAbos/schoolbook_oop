695ad33f7fb9240d35ce16f3d62f045bfc99d99b
PeterAbos <abos.petike@gmail.com>
2025-03-19 14:58:14 +0100

    model elkezdese

---
+++ app/Models/Model.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Models;
+
+use App\Database\Database;
+use App\Interfaces\ModelInterface;
+use BadFunctionCallException;
+
+abstract class Model implements ModelInterface
+{
+    public int $id;
+
+    protected $db;
+
+    protected static $table;
+
+    public function __construct()
+    {
+        $this->db = Database::getInstance();
+    }
+
+    function mapToModel(array $data): Model {
+        $model = new static();
+        foreach ($data as $key => $value) {
+            if (property_exists($model, $key)) {
+                $model->$key = $value;
+            }
+        }
+
+        return $model;
+    }
+
+    static function select(): string
+    {
+        return "SELECT * FROM `" . static::$table . "` ";
+    }
+
+    static function orderBy($orderBy = []): string
+    {
+        if (empty($orderBy)) {
+            return "";
+        }
+
+        $orderByClauses = [];
+
+        $fields = $orderBy["order_by"] ?? [];
+        $directions = $orderBy['direction'] ?? [];
+
+        foreach ($fields as $index => $field) {
+            $direction = $directions[$index] ?? 'ASC';
+            $orderByClauses[] = "$field $direction";
+        }
+
+        if (empty($orderByClauses)) {
+            return "";
+        }
+
+        return " ORDER BY " . implode(', ', $orderByClauses) . ";";
+    }
+
+    function find(int $id): ?static
+    {
+        $sql = self::select() . " WHERE id = :id";
+
+        $qryResult = $this->db->execSql($sql, ['id' => $id]);
+        if (empty($qryResult)) {
+            return null;
+        }
+
+        return $this->mapToModel($qryResult[0]);
+    }
+
+    function all($orderBy = []): array
+    {
+        $sql = self::select();
+
+        $sql .= self::orderBy($orderBy);
+
+        $qryResult = $this->db->execSql($sql);
+
+        if (empty($qryResult)) {
+            return [];
+        }
+
+        $results = [];
+        foreach ($qryResult as $row) {
+            $results[] = $this->mapToModel($row);
+        }
+
+        return $results;
+    }
+}
