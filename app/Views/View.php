a4031d0cc7462dc90f7e37678b664d413975e1ff
PeterAbos <abos.petike@gmail.com>
2025-04-02 14:00:32 +0200

    a

--- app/Views/View.php
+++ app/Views/View.php
@@ -4,7 +4,7 @@ namespace App\Views;
 
 class View
 {
-    public static function render(string $view, array $data = [], bool $useLayout) 
+    public static function render(string $view, array $data = [], bool $useLayout = true) 
     {
         $viewFile = self::resolveViewPath($view);
 
@@ -19,5 +19,20 @@ class View
 
         extract($data);
         include($viewFile);
+
+        if ($useLayout) {
+            Layout::footer();
+        }
+    }
+
+    private static function resolveViewPath(string $view) : string 
+    {
+        return __DIR__ . DIRECTORY_SEPARATOR . "$view.php";
+    }
+
+    private static function handleMissingView(string $viewFile): void 
+    {
+        error_log("View not found: $viewFile");
+        Display::message("View '$viewFile' not found.", 'error');
     }
 }
